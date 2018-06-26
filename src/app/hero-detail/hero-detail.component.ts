3b13690dba0f83f59df6a480b1f30e7229a48f69
manik.chandra <manik.chandra@bs-23.net>
2018-06-26 19:12:21 +0600

    Hero Details

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Hero} from '../ViewModel/hero';
+import {HEROES} from '../mock-heroes';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+
+  constructor() {
+  }
+
+  @Input() hero: Hero;
+
+  ngOnInit() {
+  }
+
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from '../ViewModel/hero';
+import {HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -10,28 +11,12 @@ export class HeroesComponent implements OnInit {
 
   constructor() {
   }
-
-  heroes: Hero[] = [
-    {id: 11, name: 'Mr. Nice'},
-    {id: 12, name: 'Narco'},
-    {id: 13, name: 'Bombasto'},
-    {id: 14, name: 'Celeritas'},
-    {id: 15, name: 'Magneta'},
-    {id: 16, name: 'RubberMan'},
-    {id: 17, name: 'Dynama'},
-    {id: 18, name: 'Dr IQ'},
-    {id: 19, name: 'Magma'},
-    {id: 20, name: 'Tornado'}
-  ];
-
+  heroes = HEROES;
   selectedHero: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    console.log("Selected Clicked" + hero.name);
-  }
-
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
